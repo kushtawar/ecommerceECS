db70d07813ed9e24135b134027c04d8ea761626e
Chandan Kushtawar <kushtawar@gmail.com>
2023-10-20 00:27:57 +0200

    For Build Check

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
@@ -10,11 +11,27 @@ const port = process.env.PORT || 5000;
 connectDB(); //connect to MongoDB
 
 const app = express();
-app.get('/api', (req, res) => {
+/* app.get('/api', (req, res) => {
   res.send('API is running..');
-});
+}); */
 
 app.use('/api/products', productRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
